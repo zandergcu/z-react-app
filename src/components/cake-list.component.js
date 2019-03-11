c5776cc394f9bd631863e5875e5e6fca14037483
Zander <amacke201@caledonian.ac.uk>
2019-03-11 08:25:24 +0000

    react router added

---
+++ src/components/cake-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CakeList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Cake List Component</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/review-cake.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ReviewCake extends Component {
+  render() {
+    return (
+      <div>
+        <p>Review Cake Component</p>
+      </div>
+    )
+  }
+}
