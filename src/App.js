cf48ec1105ff1ebdce86b693d9f6174aebdd9db1
Zander <amacke201@caledonian.ac.uk>
2019-03-11 08:25:24 +0000

    react router added, bootstrap added, gulp and scss files added and just to be set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CakeList from "./components/cake-list.component";
+import ViewCake from "./components/view-cake.component";
+import ReviewCake from "./components/review-cake.component";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="container">
+          <h2>Zander React App</h2>
+          <a href="/">Cake List</a><br/>
+          <a href="/view/1">View Cake</a><br/>
+          <a href="/review">Review Cake</a>
+          <Route path="/" exact component={CakeList} />
+          <Route path="/view/:id" exact component={ViewCake} />
+          <Route path="/review" exact component={ReviewCake} />
+        </div>
+      </Router>
+
     );
   }
 }
---
+++ src/components/view-cake.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class ViewCake extends Component {
+  render() {
+    return (
+      <div>
+        <p>View Cake Component</p>
+      </div>
+    )
+  }
+}
